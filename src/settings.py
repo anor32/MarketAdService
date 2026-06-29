113a69c7b3625896b870f004bd9eddfbf3d58bc4
anor23 <burgchiken@gmail.com>
2026-06-29 13:24:19 +0300

    field validation to POSTGRES_CONNECTION_String

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,6 @@
+import re
+
+from pydantic import field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -13,5 +16,13 @@ class Settings(BaseSettings):
     KAFKA_TOPIC_MARKETPLACE_ADS: str = "ads"
     AUTH_SERVICE_URL: str = "http://localhost:8000"
 
+    @field_validator("POSTGRES_CONNECTION_STRING", mode="before")
+    @classmethod
+    def ensure_asyncpg_driver(cls, v: str) -> str:
+        if isinstance(v, str):
+            if v.startswith("postgres://") or v.startswith("postgresql://"):
+                v = re.sub(r"^postgres(?:ql)?://", "postgresql+asyncpg://", v)
+        return v
 
-print(Settings().POSTGRES_CONNECTION_STRING)
+
+
